47584e01e9610aa69c8da665aa1a41b150eb26ba
Rishabh Hasija <rishabh.hasija@iubh.de>
2020-10-17 20:07:26 +0530

    Somehow got the local notification to work. It still needs to be connected to the task title

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/models/tasks.dart';
 import 'package:todo/provider/todo_model.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:todo/screens/home_screen.dart';
 import 'package:todo/screens/widgets/time_button.dart';
+import 'package:timezone/timezone.dart';
 
 class AddTaskScreen extends StatefulWidget {
   @override
@@ -15,15 +18,33 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   final taskTitleController = TextEditingController();
   bool completedStatus = false;
   bool setReminder = false;
-  String _date = "Today";
-  String _time = "Not set";
+  String _dateString = "Today";
+  String _timeString = "Not set";
+  DateTime _dateTime;
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    DateTime now = DateTime.now();
-    _time = DateFormat('kk:mm').format(now);
+
+    _dateTime = DateTime.now();
+    _timeString = DateFormat('kk:mm').format(_dateTime);
+
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('flutter_devs');
+    var initializationSettingsIOS = IOSInitializationSettings();
+    var initSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(initSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future<dynamic> onSelectNotification(String payload) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) => HomeScreen()));
   }
 
   @override
@@ -46,6 +67,43 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
       Provider.of<TodoModel>(context, listen: false).addTodo(todo);
       Navigator.pop(context);
     }
+  }
+
+  Future<void> scheduleNotification() async {
+    Location location = Location("IST", [], [], []);
+    TZDateTime scheduleNotificationDateTime = TZDateTime(
+      location,
+      _dateTime.year,
+      _dateTime.month,
+      _dateTime.day,
+      _dateTime.hour,
+      _dateTime.minute,
+    );
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel id',
+      'channel name',
+      'channel description',
+      icon: 'flutter_devs',
+      largeIcon: DrawableResourceAndroidBitmap('flutter_devs'),
+    );
+    var iosPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iosPlatformChannelSpecifics);
+
+    var uiLocalNotificationDateInterpretation =
+        UILocalNotificationDateInterpretation.absoluteTime;
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Reminder',
+      'scheduled body',
+      scheduleNotificationDateTime,
+      platformChannelSpecifics,
+      uiLocalNotificationDateInterpretation:
+          uiLocalNotificationDateInterpretation,
+      androidAllowWhileIdle: true,
+    );
   }
 
   @override
@@ -93,17 +151,19 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                                   maxTime: DateTime(2022, 12, 31),
                                   onConfirm: (dateTime) {
                                 print("Confirm: $dateTime");
+                                _dateTime = dateTime;
                                 if (dateTime.day == DateTime.now().day) {
-                                  _date = "Today";
+                                  _dateString = "Today";
                                 } else {
-                                  _date =
+                                  _dateString =
                                       '${dateTime.day}-${dateTime.month}-${dateTime.year}';
                                 }
-                                if(dateTime.minute < 10) {
-                                  _time = '${dateTime.hour}:0${dateTime.minute}';
-                                }
-                                else {
-                                  _time = '${dateTime.hour}:${dateTime.minute}';
+                                if (dateTime.minute < 10) {
+                                  _timeString =
+                                      '${dateTime.hour}:0${dateTime.minute}';
+                                } else {
+                                  _timeString =
+                                      '${dateTime.hour}:${dateTime.minute}';
                                 }
                                 setState(() {});
                               },
@@ -121,7 +181,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                               child: Row(
                                 children: [
                                   Icon(Icons.access_time),
-                                  Text("$_date $_time"),
+                                  Text("$_dateString $_timeString"),
                                 ],
                               ),
                             ),
@@ -134,6 +194,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                   RaisedButton(
                     child: Text("Add"),
                     onPressed: () {
+                      scheduleNotification();
                       onAdd();
                     },
                   )
